1cd22a6487589bba9f2e6e96029bb38109011114
Yihan Chen <yihanc92@live.se>
2019-01-20 12:45:09 +0100

    Updated visualization routines

--- scripts/visualize_data.py
+++ scripts/visualize_data.py
@@ -11,6 +11,19 @@ if __name__ == '__main__':
         input_dir / 'train.csv',
         dtype={'acoustic_data': np.int16, 'time_to_failure': np.float64})
 
-    train[train.columns[1]].plot()
-    plt.savefig('data')
+    plt.figure(figsize=(50,30))
+    train[train.columns[1]].plot(grid=True,label=train.columns[1])
+    train[train.columns[0]].plot(grid=True,label=train.columns[0])
+    
+    plt.legend()
+    plt.savefig('data_raw')
     plt.show()
+    
+    
+    #preparations for LTA/STA processsed data
+    plt.figure(figsize=(50,30))
+    plt.plot(train[train.columns[0]],train[train.columns[1]],'.',color='b')
+    linje=np.linspace(0,16,17)
+    plt.plot(linje,linje,color='r')
+    plt.savefig('data_scatter')
+    
