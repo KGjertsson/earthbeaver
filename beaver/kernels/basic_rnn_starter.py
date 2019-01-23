402a5472b3c57c72c919586f5ab938ed49c92fac
Konrad Gjertsson <gjertsson.konrad@gmail.com>
2019-01-23 20:53:01 +0100

    small updates

--- beaver/kernels/basic_rnn_starter.py
+++ beaver/kernels/basic_rnn_starter.py
@@ -1,7 +1,7 @@
 # source: https://www.kaggle.com/mayer79/rnn-starter
 from time import time
 
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense, CuDNNGRU
 from keras.optimizers import adam
 from keras.callbacks import ModelCheckpoint
@@ -10,9 +10,11 @@ import numpy as np
 import pandas as pd
 
 
-# Helper function for the data generator. Basically we want to extract
-# mean, standard deviation, min and max per time step.
 def extract_features(z):
+    """
+    Helper function for the data generator. Basically we want to extract mean,
+    standard deviation, min and max per time step.
+    """
     return np.c_[z.mean(axis=1),
                  np.median(np.abs(z), axis=1),
                  z.std(axis=1),
@@ -25,22 +27,21 @@ def extract_features(z):
 # From each piece, 16 features are extracted. This results in a feature
 # matrix of dimension (150 time steps x 16 features).
 def create_x(x, last_index=None, n_steps=150, step_length=1000):
-    if last_index == None:
+    if not last_index:
         last_index = len(x)
-
     assert last_index - n_steps * step_length >= 0
 
     # Reshaping and approximate standardization with mean 5 and std 3.
-    temp = (x[(last_index - n_steps * step_length):last_index].reshape(
+    feature_matrix = (x[
+                      (last_index - n_steps * step_length):last_index].reshape(
         n_steps, -1) - 5) / 3
 
     # Extracts features of sequences of full length 1000, of the last 100
-    # values and finally also
-    # of the last 10 observations.
-    return np.c_[extract_features(temp),
-                 extract_features(temp[:, -step_length // 10:]),
-                 extract_features(temp[:, -step_length // 100:]),
-                 temp[:, -1:]]
+    # values and finally also of the last 10 observations.
+    return np.c_[extract_features(feature_matrix),
+                 extract_features(feature_matrix[:, -step_length // 10:]),
+                 extract_features(feature_matrix[:, -step_length // 100:]),
+                 feature_matrix[:, -1:]]
 
 
 # The generator randomly selects "batch_size" ending positions of sub-time
@@ -101,18 +102,20 @@ def init_generators(train_data, n_features, batch_size):
 
 
 def make_model(n_features):
-    # Define model
-
+    print('Creating model...')
+    t_make_model = time()
     model = Sequential()
-    model.add(CuDNNGRU(48, input_shape=(None, n_features)))
+    model.add(CuDNNGRU(64, return_sequences=True,
+                       input_shape=(None, n_features)))
+    model.add(CuDNNGRU(64))
     model.add(Dense(10, activation='relu'))
     model.add(Dense(1))
 
+    # TODO: investigate linear activation function in final layer
     model.summary()
 
-    # Compile and fit model
-    model.compile(optimizer=adam(lr=0.0005), loss="mae")
-
+    model.compile(optimizer=adam(lr=5e-4), loss="mae")
+    print('Done {} s'.format(time() - t_make_model))
     return model
 
 
@@ -153,9 +156,42 @@ def make_submission(input_dir, model):
 # We call "extract_features" three times, so the total number of features
 # is 3 * 5 + 1 (last value) = 16
 def run_kernel(input_dir, n_features=16, batch_size=32):
-    train_data = load_train_data(input_dir)
-    train_gen, valid_gen = init_generators(train_data, n_features, batch_size)
-    model = make_model(n_features)
-    history = perform_training(model, train_gen, valid_gen)
-    plot_training_history(history)
-    make_submission(input_dir, model)
+    train_model = False
+
+    if train_model:
+        train_data = load_train_data(input_dir)
+        train_gen, valid_gen = init_generators(train_data, n_features,
+                                               batch_size)
+        model = make_model(n_features)
+        history = perform_training(model, train_gen, valid_gen)
+        # plot_training_history(history)
+        make_submission(input_dir, model)
+
+        print('Min val loss: {} at epoch: {}'.format(
+            np.min(history.history['val_loss']),
+            np.argmin(history.history['val_loss'])))
+    else:
+        train_data = load_train_data(input_dir)
+        model = load_model('model.hdf5')
+        train_data_length = len(train_data)
+        train_predictions = pd.DataFrame()
+
+        for i in range(0, train_data_length, 150000):
+            end_index = i + 150000
+            if i + 150000 > train_data_length:
+                end_index = train_data_length - 1
+
+            train_features = create_x(train_data[i:end_index])
+            train_features = np.expand_dims(train_features, 0)
+            prediction = model.predict(train_features)
+
+            train_predictions.append(
+                {end_index: prediction},
+                ignore_index=True)
+
+            if i == 0:
+                train_predictions.to_csv('tiny_retard')
+        train_predictions.to_csv('train_predictions')
+        print(train_predictions)
+
+# TODO: add https://en.wikipedia.org/wiki/Nash%E2%80%93Sutcliffe_model_efficiency_coefficient as loss
