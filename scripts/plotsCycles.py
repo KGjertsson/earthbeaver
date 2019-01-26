5b769bdfa53ae23073b9032df9e87afc6fa760ad
Yihan Chen <yihanc92@live.se>
2019-01-26 10:35:00 +0100

    plotcycles updated

--- scripts/plotsCycles.py
+++ scripts/plotsCycles.py
@@ -8,18 +8,32 @@ Created on Sat Jan 19 13:04:56 2019
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from pathlib import Path
+
 
 print('loading file...')
-filename="C:\\Users\\Yihan Chen\\Desktop\\TestData\\cycle9.csv"
 
+
+
+files = [currentpath for currentpath in Path('C:\\earthbeaver\\data\\lanl\\cycles').glob('*')]
 pd.set_option("display.precision", 15)
 
-train = pd.read_csv(filename)
-print('done')
+for file in files:
+    df = pd.read_csv(file)
+    plt.figure(figsize=(20,10))
+    print('plotting'+str(file)[32:-4])
+    df[df.columns[1]].plot(grid=True, label=df.columns[1])
+    #df[df.columns[2]].plot(grid=True, label=df.columns[2])
+    plt.savefig('C:\\'+str(file)[32:-4])
+    
+    plt.close('all')
+    plt.legend()
+    
 
 
-plt.figure(figsize=(20,10))
-train[train.columns[1]].plot()
+
+
+
 
 
 
